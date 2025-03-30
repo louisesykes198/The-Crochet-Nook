433a96e2a3f331ae67f1a4a860c01a0ec34232f0
louisesykes198 <louisesykes198@yahoo.com>
2025-03-30 12:20:02 +0100

    Add views, urls and html for the login page

--- crochet/views.py
+++ crochet/views.py
@@ -65,6 +65,9 @@ from django.http import HttpResponse
 from .models import Project
 from .forms import ProjectForm
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 
 
@@ -129,6 +132,21 @@ def register(request):
 
     return render(request, 'register.html', {'form': form})  # Render the register page
 
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirect to home after successful login
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
 
-
-
+    return render(request, 'login.html', {'form': form})
