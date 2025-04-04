8823928e817e564cfe07249cd791a72736688b60
louisesykes198 <louisesykes198@yahoo.com>
2025-04-04 15:30:39 +0100

    Add django-heroku settings at the bottom

--- crochet/admin.py
+++ crochet/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Project, Comment, Like, Pattern
+
+admin.site.register(Project)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Pattern)
